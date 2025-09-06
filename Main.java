f3bc08350234e50486cdf10901cdd3ed558f1172
Benjamin Navarro <bnavarro21@alumnos.utalca.cl>
2025-09-06 16:05:34 -0400

    main

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
-
 import java.util.*;
+import java.util.stream.IntStream;
 
 public class Main {
 
@@ -9,80 +9,88 @@ public class Main {
             return;
         }
 
-        int numHormigas= Integer.parseInt(args[0]);     
-        double evaporacion= Double.parseDouble(args[1]);  
-        int maxIter= Integer.parseInt(args[2]);     
+        int numHormigas = Integer.parseInt(args[0]);
+        double evaporacion = Double.parseDouble(args[1]);
+        int maxIter = Integer.parseInt(args[2]);
 
-        double alpha= 1.0;   
-        double beta = 5.0;   
-        double Q= 500.0; 
+        double alpha = 10.0;
+        double beta = 1.0;
+        double Q = 500.0;
 
         List<Ciudad> ciudades = LeerArchivo.cargarCiudades("a280.tsp");
         int[][] dist = UtilidadesTSP.matrizDistancias(ciudades);
-        double[][] tau = UtilidadesTSP.inicializarFeromonas(ciudades.size(), 1.0);
-
-        Random rand = new Random();
+        double[][] tau = UtilidadesTSP.inicializarFeromonas(ciudades.size(), 0.1);
 
         int mejorLongitud = Integer.MAX_VALUE;
+        int[] mejorTour = null;
+
+        Random randGlobal = new Random();
 
         for (int it = 0; it < maxIter; it++) {
+            final int iter = it;
+            
+            int[][] tours = new int[numHormigas][];
+            int[] longitudes = new int[numHormigas];
 
-            List<int[]> tours = new ArrayList<>();
-            List<Integer> longitudes = new ArrayList<>();
-
-            for (int k = 0; k < numHormigas; k++) {
+            IntStream.range(0, numHormigas).parallel().forEach(k -> {
+                Random rand = new Random(randGlobal.nextLong() + k + iter * numHormigas);
                 int[] tour = UtilidadesTSP.construirTour(ciudades.size(), tau, dist, alpha, beta, rand);
                 int longitud = UtilidadesTSP.distanciaRecorrida(tour, dist);
+                tours[k] = tour;
+                longitudes[k] = longitud;
+            });
 
-                tours.add(tour);
-                longitudes.add(longitud);
-
-                if (longitud < mejorLongitud) {
-                    mejorLongitud = longitud;
+            for (int k = 0; k < numHormigas; k++) {
+                if (longitudes[k] < mejorLongitud) {
+                    mejorLongitud = longitudes[k];
+                    mejorTour = Arrays.copyOf(tours[k], tours[k].length);
                 }
             }
 
+            double factorEvaporacion = 1 - evaporacion;
             for (int i = 0; i < tau.length; i++) {
                 for (int j = 0; j < tau.length; j++) {
-                    tau[i][j] *= (1 - evaporacion);
+                    tau[i][j] *= factorEvaporacion;
                 }
             }
 
-            for (int a = 0; a < tours.size(); a++) {
-                int[] tour = tours.get(a);
-                int longitud = longitudes.get(a);
-                double delta = Q / longitud;
-
+            for (int k = 0; k < numHormigas; k++) {
+                double delta = Q / longitudes[k];
+                int[] tour = tours[k];
                 for (int paso = 0; paso < tour.length - 1; paso++) {
                     int i = tour[paso];
                     int j = tour[paso + 1];
                     tau[i][j] += delta;
                     tau[j][i] = tau[i][j];
                 }
-
                 int u = tour[tour.length - 1];
                 int v = tour[0];
                 tau[u][v] += delta;
                 tau[v][u] = tau[u][v];
+            }
 
-                int progreso = (int) ((it + 1) / (double) maxIter * 50); 
-                if ((it + 1) % 10 == 0 || it + 1 == maxIter) { 
-                    
-                    System.out.print("\rProgreso: [");
-
-                    for (int i = 0; i < 50; i++) {
-                        if (i < progreso) {
-                            System.out.print("=");
-                        } else {
-                            System.out.print(" ");
-                        }
+            int progreso = (int) ((it + 1) / (double) maxIter * 50);
+            if ((it + 1) % 10 == 0 || it + 1 == maxIter) {
+                System.out.print("\rProgreso: ");
+                for (int i = 0; i < 50; i++) {
+                    if (i < progreso) {
+                        System.out.print("#");
+                    } else {
+                        System.out.print(" ");
                     }
-                    System.out.print("] " + (it + 1) + "/" + maxIter + " Mejor hasta ahora: " + mejorLongitud);
                 }
-                
+                System.out.print(" " + (it + 1) + "/" + maxIter + " Mejor hasta ahora: " + mejorLongitud);
+            }
 
-                if (it + 1 == maxIter) {
-                    System.out.println("\n\nMejor longitud encontrada: " + mejorLongitud);
+            if (it + 1 == maxIter) {
+                System.out.println("\n\nMejor longitud encontrada: " + mejorLongitud);
+                System.out.print("Mejor tour encontrado: ");
+                if (mejorTour != null) {
+                    for (int i = 0; i < mejorTour.length; i++) {
+                        System.out.print(mejorTour[i]);
+                        if (i < mejorTour.length - 1) System.out.print(" -> ");
+                    }
+                    System.out.println();
                 }
             }
         }
